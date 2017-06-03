4938fa5b140fe33c03325fca381dcd19bba659cb
Moritz Mueller <moritz.mueller@posteo.de>
2017-06-03 16:53:21 +0200

    added both projects! - testing other project structure

---
+++ EMA03.1_Aktienkurse/EMA03.1_Aktienkurse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMA03.1_Aktienkurse
+//
+//  Created by Moritz Müller on 03.06.17.
+//  Copyright © 2017 Moritz Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
